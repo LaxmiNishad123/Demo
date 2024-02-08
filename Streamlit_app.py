8c7ebff28eb6b7373e44cbcd49a77c699096a9f2
LaxmiNishad123 <159414728+LaxmiNishad123@users.noreply.github.com>
2024-02-08 15:23:40 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -3,20 +3,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-csv_file_path = "C:\\Users\\User22\\Desktop\\Deployment_913\\Data\\Iris.csv"
 
-df=pd.read_csv(csv_file_path)
 st.title('IRIS FLOWER DASHBOARD')
 st.sidebar.subheader('Description')
 st.divider()
 col1,col2=st.columns(2) 
 with col1:
     st.header('Pie Chart of Species')
-    class_data=df['class'].value_counts()
-    fig, ax = plt.subplots()
-    ax.pie(class_data,labels=['Setosa','versicolor','virginica'])
-    st.pyplot(fig)
+   
 with col2:
     st.header('Bar Chart of Species')
-    st.pyplot(df)
+   
 
